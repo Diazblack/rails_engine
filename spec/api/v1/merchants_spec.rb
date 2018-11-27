2861218ec59d7ee9d6a71ed78b896474580413b5
Cesar Jolibois <diazcd85@gmail.com>
2018-11-26 18:31:46 -0700

    successfully render a json for Merchants index

--- spec/api/v1/merchants_spec.rb
+++ spec/api/v1/merchants_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 describe 'Mechants' do
   it 'should send a list with all merchants' do
-    create_list(:merchant, 10)
+    mer_1, mer_2, mer_3, mer_4 = create_list(:merchant, 4)
 
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(Merchant.all.count).to eq(4)
+    expect(Merchant.first.name).to eq(mer_1.name)
+    expect(Merchant.last.name).to eq(mer_4.name)
+
   end
 end
