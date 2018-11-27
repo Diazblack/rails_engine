7caee1dfeff1abf883219c8921e237a33f5cd8fa
Cesar Jolibois <diazcd85@gmail.com>
2018-11-27 15:06:52 -0700

    modification on the migration files to enable citext extension

--- db/migrate/20181127010833_create_merchants.rb
+++ db/migrate/20181127010833_create_merchants.rb
@@ -1,6 +1,7 @@
 class CreateMerchants < ActiveRecord::Migration[5.1]
   def change
     create_table :merchants do |t|
+      enable_extension :citext
       t.citext :name
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,17 @@ ActiveRecord::Schema.define(version: 20181127210154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "citext"
 
   create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.citext "first_name"
+    t.citext "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "merchants", force: :cascade do |t|
-    t.string "name"
+    t.citext "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
