4d7b101c7f1cb1a989042d7193329c61038d4737
Cesar Jolibois <diazcd85@gmail.com>
2018-11-26 18:19:57 -0700

    create Merchant model, added index route for merchants

--- spec/api/v1/merchants_spec.rb
+++ spec/api/v1/merchants_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Mechants' do
   it 'should send a list with all merchants' do
-    create_list(:merchants, 10)
+    create_list(:merchant, 10)
 
     get '/api/v1/merchants'
 
