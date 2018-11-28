58d811020a8987edd9d0070d3f793b93a8d7cb73
Cesar Jolibois <diazcd85@gmail.com>
2018-11-27 19:59:12 -0700

    Able to find all  the merchants created at the same time

--- spec/requests/api/v1/merchant_query_params_spec.rb
+++ spec/requests/api/v1/merchant_query_params_spec.rb
@@ -46,20 +46,23 @@ describe "Merchant find parameters" do
 
   end
 
-  # it "should find_all merchant by created_at " do
-  #   mer_1, mer_2, mer_3, mer_4 = create_list(:merchant, 4)
-  #   mer_5 = create(:merchant, name: "Plutark")
-  #   mer_6 = create(:merchant, name: "plutark")
-  #
-  #   get "/api/v1/merchants/find_all?name=Plutark"
-  #
-  #   expect(response).to be_successful
-  #
-  #   merchant = JSON.parse(response.body)
-  #
-  #   expect(merchant.count).to eq(2)
-  #   expect(merchant.last["id"]).to eq(mer_6.id)
-  #
-  # end
+  it "should find_all merchant by created_at " do
+    mer_1 = create(:merchant, created_at: Time.now)
+    mer_2 = create(:merchant, created_at: "2012-03-27 14:54:05 UTC" )
+    mer_3 = create(:merchant, created_at: "2012-03-27 14:54:05 UTC" )
+    mer_4 = create(:merchant, created_at: "2012-03-27 14:54:05 UTC" )
+
+    get "/api/v1/merchants/find_all?created_at=2012-03-27 14:54:05 UTC"
+
+    expect(response).to be_successful
+
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants.count).to eq(3)
+    expect(merchants.first["created_at"]).to eq("2012-03-27T14:54:05.000Z")
+    expect(merchants.last["created_at"]).to eq("2012-03-27T14:54:05.000Z")
+
+  end
 
 end
