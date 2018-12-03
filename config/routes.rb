07dbd25082859c25824d82a8bc854eea5d4bd55a
Cesar Jolibois <diazcd85@gmail.com>
2018-12-02 18:00:37 -0700

    added functionality to get information of a merchant favorite customer

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
+        get ':id/favorite_customer', to: 'customer#show'
         get ':id/revenue', to: 'revenue#show'
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
